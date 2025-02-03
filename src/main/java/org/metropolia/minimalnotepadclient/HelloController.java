af7eafcc6befa3409baa5698c5a696d0b0d65281
Runzhou Zhu <runzhouz@metropolia.fi>
2025-02-03 14:11:25 +0200

    edit the hello page

--- src/main/java/org/metropolia/minimalnotepadclient/HelloController.java
+++ src/main/java/org/metropolia/minimalnotepadclient/HelloController.java
@@ -2,13 +2,27 @@ package org.metropolia.minimalnotepadclient;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label logIn;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void loginClicked() {
+        System.out.println("Login clicked");
+    }
+
+    @FXML
+    public void loginMouseEnter() {
+        logIn.setTextFill(Color.GRAY);
+    }
+
+    @FXML
+    public void loginMouseExit() {
+        logIn.setTextFill(Color.BLACK);
     }
 }
