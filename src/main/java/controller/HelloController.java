a4b9b7e29c4343fe8c43d46cad6f0e73948ff063
Runzhou Zhu <runzhouz@metropolia.fi>
2025-02-03 17:58:53 +0200

    edit hello page

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -3,24 +3,24 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import view.LogInView;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
     @FXML
     private Label logIn;
 
+    private Stage stage;
+
     @FXML
-    protected void loginClicked() throws IOException {
-        Stage stage = (Stage) logIn.getScene().getWindow();
-        Parent root = LogInView.loadLogInView();
-
-        stage.setScene(root.getScene());
-
+    protected void loginClicked(){
+        System.out.println("addButtonClicked() called");
+        updateStage();
     }
 
     @FXML
@@ -31,5 +31,26 @@ public class HelloController {
     @FXML
     public void loginMouseExit() {
         logIn.setTextFill(Color.BLACK);
+    }
+
+
+    private Stage getStage() {
+        if (stage == null) {
+            stage = (Stage) logIn.getScene().getWindow();
+        }
+        return stage;
+    }
+
+    private void updateStage() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/logIn_view.fxml"));
+            Parent root = fxmlLoader.load();
+
+            var stage = getStage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
