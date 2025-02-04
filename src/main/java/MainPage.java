c06ff583c313f69d76be816799d8ef3aa518a371
Runzhou Zhu <runzhouz@metropolia.fi>
2025-02-04 20:24:40 +0200

    edit the main page

---
+++ src/main/java/MainPage.java
@@ -0,0 +1,7 @@
+import view.MainPageView;
+
+public class MainPage {
+    public static void main(String[] args) {
+        MainPageView.launch(MainPageView.class);
+    }
+}
---
+++ src/main/java/controller/MainPageController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainPageController {
+
+
+
+    @FXML
+    private void buttonMouseEnter() {
+
+    }
+
+}
