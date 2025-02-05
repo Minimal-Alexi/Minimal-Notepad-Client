e649a903a72657175c1f75e5bdbd4a73966ab966
Runzhou Zhu <runzhouz@metropolia.fi>
2025-02-05 15:30:40 +0200

    edit the edit note page

--- src/main/java/controller/EditNoteController.java
+++ src/main/java/controller/EditNoteController.java
@@ -1,6 +1,29 @@
 package controller;
 
-public class EditNoteController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+import java.awt.*;
+import java.util.Collection;
+
+public class EditNoteController{
+
+    @FXML
+    private VBox textVBox;
+
+    public void addTextArea(MouseEvent mouseEvent) {
+        textVBox.setSpacing(10);
+        TextArea textArea = new TextArea();
+        textVBox.getChildren().add(textArea);
+    }
 
 
+
+    public void saveNoteClicked(ActionEvent event) {
+
+    }
 }
--- src/main/java/controller/MainPageController.java
+++ src/main/java/controller/MainPageController.java
@@ -2,12 +2,21 @@ package controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.Note;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MainPageController {
     @FXML
@@ -22,6 +31,10 @@ public class MainPageController {
     private TableColumn<Note, String> category;
     @FXML
     private TableColumn<Note, String> createTime;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     private final ObservableList<Note> notes = FXCollections.observableArrayList(
             new Note("Illustration packs", "Product needs", "Kurnia Majid", "Hobby", "Apr 10, 2022"),
@@ -39,4 +52,11 @@ public class MainPageController {
         createTime.setCellValueFactory(new PropertyValueFactory<Note, String>("createTime"));
     }
 
+    public void newNoteClicked(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/main_pages/edit_note_page.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
