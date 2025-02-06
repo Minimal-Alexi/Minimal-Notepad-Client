533b1536397515bae6946157409f59c6b307d650
Runzhou Zhu <runzhouz@metropolia.fi>
2025-02-06 09:25:22 +0200

    can copy and paste pictures now

--- src/main/java/controller/EditNoteController.java
+++ src/main/java/controller/EditNoteController.java
@@ -2,28 +2,55 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.TextArea;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 
-import java.awt.*;
-import java.util.Collection;
 
-public class EditNoteController{
+public class EditNoteController {
 
     @FXML
     private VBox textVBox;
 
-    public void addTextArea(MouseEvent mouseEvent) {
-        textVBox.setSpacing(10);
-        TextArea textArea = new TextArea();
-        textVBox.getChildren().add(textArea);
+    @FXML
+    private TextArea textArea1;
+
+    public void textAreaKeyPressed(KeyEvent keyEvent) {
+        /*
+            When the user press control + v, the app creates a imageView and insert a picture into it.
+            Then create a new text area.
+        */
+        if (keyEvent.isControlDown() && keyEvent.getCode() == KeyCode.V) {
+            // Get the content of the clipboard
+            Clipboard clipboard = Clipboard.getSystemClipboard();
+
+            if (clipboard.hasImage()) {
+                // Add an image and imageView to the Vbox
+                Image image = clipboard.getImage();
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(500);
+                imageView.setPreserveRatio(true);
+
+                textVBox.getChildren().add(imageView);
+
+                // Add a new textArea
+                TextArea textArea = new TextArea();
+                textArea.addEventHandler(KeyEvent.KEY_PRESSED, this::textAreaKeyPressed);
+                textVBox.getChildren().add(textArea);
+            }
+        }
     }
-
-
 
     public void saveNoteClicked(ActionEvent event) {
 
     }
+
+    /*
+    Below is the tool methods:
+     */
+
 }
