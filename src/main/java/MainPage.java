b0bdc49642f4a463d3c5968e4ee676c168daeaf1
Runzhou Zhu <runzhouz@metropolia.fi>
2025-02-24 19:44:26 +0200

    123

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -4,7 +4,7 @@ import view.MainPageView;
 public class MainPage {
     public static void main(String[] args) {
         TokenStorage.getIntance();
-        TokenStorage.saveToken("username", "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VybmFtZSIsImlhdCI6MTc0MDQwNTI0NiwiZXhwIjoxNzQwNDkxNjQ2fQ.zjOHjdkeF6DPCzGZgFEZQOUM9JsqWwluYTYPmN3S7Ao");
+        TokenStorage.saveToken("username", "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VybmFtZSIsImlhdCI6MTc0MDQxNjI1MSwiZXhwIjoxNzQwNTAyNjUxfQ.SS87KCKD9XNk5GaSHE7DhtQxXFFrGySu2worRlME9Ks");
 
         MainPageView.launch(MainPageView.class);
     }
--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,25 +1,38 @@
 package controller;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import model.TokenStorage;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import utils.ControllerUtils;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class HelloController {
+    private static final Log log = LogFactory.getLog(HelloController.class);
     @FXML
     private Label logIn;
 
     @FXML
     private Label registerLabel;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private VBox loginVBox;
 
     private Stage stage;
     private ControllerUtils controllerUtil;
@@ -64,6 +77,23 @@ public class HelloController {
     public void initialize() {
         TokenStorage.getIntance();
         this.controllerUtil = new ControllerUtils();
+
+        /*
+        Set window size UI adaptation
+         */
+        Platform.runLater(() -> {
+            imageView.getScene().getWindow().widthProperty().addListener((observable, oldValueWidth, newValueWidth) -> {
+                imageView.setFitWidth((Double) newValueWidth);
+
+                if ((Double) newValueWidth > 700.0) {
+                    AnchorPane.setBottomAnchor(loginVBox, 0.0);
+                    AnchorPane.setRightAnchor(loginVBox, 0.0);
+                } else {
+                    AnchorPane.setBottomAnchor(loginVBox, -80.0);
+                    AnchorPane.setLeftAnchor(loginVBox, 0.0);
+                }
+            });
+        });
     }
 
     private Stage getStage() {
