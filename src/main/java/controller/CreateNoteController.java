a7d864d528c51d6370272af2e98552fcb1b9f5f3
Runzhou Zhu <runzhouz@metropolia.fi>
2025-02-25 16:14:26 +0200

    update create note page ui

--- src/main/java/controller/CreateNoteController.java
+++ src/main/java/controller/CreateNoteController.java
@@ -1,23 +1,15 @@
 package controller;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.Clipboard;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.Note;
 import model.TokenStorage;
@@ -52,27 +44,7 @@ public class CreateNoteController {
     public void initialize() {
         updateLocalTime(localTime);
         updateNameLabel(nameLabel, TokenStorage.getUser());
-
-        /*
-        Add some new event handlers
-         */
-        // press ENTER to create a new textArea
-        // This is a duplicated code and should be
-        textArea1.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
-            if (event.getCode().toString().equals("ENTER")) {
-                event.consume(); // prevent the default enter warping behavior
-
-                TextArea newTextArea = createTextArea();
-                VBox parent = (VBox) textArea1.getParent();
-                parent.getChildren().add(parent.getChildren().indexOf(textArea1) + 1, newTextArea);
-
-                newTextArea.requestFocus(); // move the cursor to the next textArea
-            }
-        });
         colorSetUp();
-
-        TextArea textArea = createTextArea();
-        textVBox.getChildren().add(textArea);
     }
 
     public void saveNoteClicked(ActionEvent event) throws IOException {
@@ -125,83 +97,6 @@ public class CreateNoteController {
         addCategory.setDisable(false);
     }
 
-    /*
-    ctrl+v insert picture
-     */
-    public void textAreaKeyPressed(KeyEvent keyEvent) {
-        /*
-            When the user press control + v, the app creates a imageView and insert a picture into it.
-            Then create a new text area.
-        */
-        if (keyEvent.isControlDown() && keyEvent.getCode() == KeyCode.V) {
-            // Get the content of the clipboard
-            Clipboard clipboard = Clipboard.getSystemClipboard();
-
-            if (clipboard.hasImage()) {
-                // Add an image and imageView to the Vbox
-                Image image = clipboard.getImage();
-                ImageView imageView = new ImageView(image);
-                imageView.setFitWidth(500);
-                imageView.setPreserveRatio(true);
-
-                textVBox.getChildren().add(imageView);
-
-                // Add a new textArea
-                TextArea textArea = new TextArea();
-                textArea.addEventHandler(KeyEvent.KEY_PRESSED, this::textAreaKeyPressed);
-                textVBox.getChildren().add(textArea);
-            }
-        }
-    }
-
-    /*
-    press ENTER to create a new textarea
-     */
-    private TextArea createTextArea() {
-        TextArea textArea = new TextArea();
-        textArea.setWrapText(true);
-
-        // press ENTER to create a new textArea
-        textArea.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
-            if (event.getCode().toString().equals("ENTER")) {
-                event.consume(); // prevent the default enter warping behavior
-
-                TextArea newTextArea = createTextArea();
-                VBox parent = (VBox) textArea.getParent();
-                parent.getChildren().add(parent.getChildren().indexOf(textArea) + 1, newTextArea);
-
-                newTextArea.requestFocus(); // move the cursor to the next textArea
-            }
-        });
-
-        // If the textArea is empty, press BACKSPACE to delete this textArea and put the cursor into the textArea above
-        textArea.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
-            if (textArea.getText().isEmpty()) {
-                if (event.getCode() == KeyCode.BACK_SPACE) {
-
-                    // Move the cursor to the previous textArea
-                    VBox parent = (VBox) textArea.getParent();
-                    TextArea previousTextArea = (TextArea) parent.getChildren().get(parent.getChildren().indexOf(textArea) - 1);
-                    previousTextArea.requestFocus();
-
-                    // Delete this textArea
-                    parent.getChildren().remove(textArea);
-                }
-            }
-
-        });
-
-        // Set the height of the textArea changing with the text lines
-        textArea.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                Text text = (Text) textArea.lookup(".text");
-                textArea.setPrefHeight(text.boundsInParentProperty().get().getMaxY() + 20);
-            }
-        });
-
-        return textArea;
-    }
     /*
 
     */
--- src/main/java/controller/EditNoteController.java
+++ src/main/java/controller/EditNoteController.java
@@ -8,9 +8,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.Clipboard;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -140,35 +137,6 @@ public class EditNoteController {
 
     public void groupsClicked(ActionEvent event) throws IOException {
         goToPage(stage, scene, event, "/fxml/main_pages/groups_page.fxml");
-    }
-
-    /*
-    Unfinished functions
-     */
-    public void textAreaKeyPressed(KeyEvent keyEvent) {
-        /*
-            When the user press control + v, the app creates a imageView and insert a picture into it.
-            Then create a new text area.
-        */
-        if (keyEvent.isControlDown() && keyEvent.getCode() == KeyCode.V) {
-            // Get the content of the clipboard
-            Clipboard clipboard = Clipboard.getSystemClipboard();
-
-            if (clipboard.hasImage()) {
-                // Add an image and imageView to the Vbox
-                Image image = clipboard.getImage();
-                ImageView imageView = new ImageView(image);
-                imageView.setFitWidth(500);
-                imageView.setPreserveRatio(true);
-
-                textVBox.getChildren().add(imageView);
-
-                // Add a new textArea
-                TextArea textArea = new TextArea();
-                textArea.addEventHandler(KeyEvent.KEY_PRESSED, this::textAreaKeyPressed);
-                textVBox.getChildren().add(textArea);
-            }
-        }
     }
 
     private void colorSetUp(String initialColor) {
