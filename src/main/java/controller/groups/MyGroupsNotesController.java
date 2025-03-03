ba7246492a1f55de25c4eba18fd4a162ac4587a1
Runzhou Zhu <runzhouz@metropolia.fi>
2025-03-03 10:26:47 +0200

    my groups note added

---
+++ src/main/java/controller/groups/MyGroupsNotesController.java
@@ -0,0 +1,21 @@
+package controller.groups;
+
+import javafx.scene.input.MouseEvent;
+
+public class MyGroupsNotesController {
+    public void tableClicked(MouseEvent mouseEvent) {
+
+    }
+
+    public void groupsBtnClick(MouseEvent mouseEvent) {
+
+    }
+
+    public void accountBtnClick(MouseEvent mouseEvent) {
+
+    }
+
+    public void logOutBtnClick(MouseEvent mouseEvent) {
+
+    }
+}
